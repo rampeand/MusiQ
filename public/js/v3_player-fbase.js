5a24e00717ae0df03379f33a5da9ca834f850f90
Andre Rampersad <Andre.Rampersad@kubra.com>
2019-12-23 17:32:48 -0500

    fixed search in player feature

--- public/js/v3_player-fbase.js
+++ public/js/v3_player-fbase.js
@@ -13,6 +13,19 @@ firebase.initializeApp(firebaseConfig);
 
 const firestore = firebase.firestore();  //grab ref to firestore
 
+
+//YT API Auth
+gapi.load("client:auth2", function() {
+    gapi.auth2.init({client_id: "637157888993-l9178kevdkdrfpoqbckmgkdck4ljvjvk.apps.googleusercontent.com"});
+  });
+  
+  function loadClient() {
+    gapi.client.setApiKey("AIzaSyBKE9MzjNBkMIf3eyh69uYo8aDBc3VD_5o");
+    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
+  }
+
 function getPlayerID(){
     var playerid = document.getElementById("playerid").value;
     return playerid;
@@ -23,7 +36,7 @@ function initPlayer(getPlayerID){
     togglePlaylistVisibility()
     updatePlaylist(getPlayerID);
     loadSong();
-    //loadClient();
+    loadClient();
 }
 
 function loopPlaylist(){
